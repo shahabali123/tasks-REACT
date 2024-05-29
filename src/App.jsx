72dfaf74521c3ca693a1b8fe9953288f18a91634
shahabali123 <shahab.mughal.180@gmail.com>
2024-05-29 04:18:03 +0400

    oops

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,7 @@ function App() {
       <Route path='/about' element={<About />} />
       <Route path='/contact' element={<Contact />} />
       <Route path='/user/:id' element={<User />}/>
+      <Route path='*' element={<Home />}/>
       </Routes>
       
       <Footer />
